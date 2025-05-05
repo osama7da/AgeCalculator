179739321159f29760d04fbd4e44b7a82015a003
osama7da <osamadaoud66@gmail.com>
2025-05-06 02:06:14 +0300

    my first commit

---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Age_Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+        
+               
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBox1.Text, out int birthYear))
+            {
+                // Calculate age
+                int currentYear = DateTime.Now.Year;
+                int age = currentYear - birthYear;
+
+                // Display result
+                label3.Text = $"Your age is: {age}";
+            }
+            else
+            {
+                // Show error message
+                MessageBox.Show("Please enter a valid year.", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+    }
+}
